5af642056d8a3c7b5cf95111bd1fcd0b1f2c85e5
ThomasJRye <thomasjrye@gmail.com>
2021-05-06 10:56:34 +0200

    Ferdig?

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,5 +1,13 @@
 package no.hvl.dat110.aciotdevice.client;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import java.io.IOException;
+import com.google.gson.Gson;
+
 public class RestClient {
 
 	public RestClient() {
@@ -10,8 +18,20 @@ public class RestClient {
 
 	public void doPostAccessEntry(String message) {
 
-		// TODO: implement a HTTP POST on the service to post the message
 		
+		OkHttpClient client = new OkHttpClient();
+		RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), new Gson().toJson(new AccessMessage(message)));
+		Request request = new Request.Builder().url("http://localhost:8080" + logpath).post(body).build();
+		
+		String requestString = request.toString();
+		
+		System.out.println(requestString);
+		
+		try (Response response = client.newCall(request).execute()) {
+			System.out.println(response.body().string());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private static String codepath = "/accessdevice/code";
@@ -20,8 +40,27 @@ public class RestClient {
 
 		AccessCode code = null;
 		
-		// TODO: implement a HTTP GET on the service to get current access code
+		OkHttpClient client = new OkHttpClient();
+		client.newBuilder().build();
+		Request request = new Request.Builder().url("http://localhost:8080" + codepath).get().build();
+
+		String requestString = request.toString();
+		System.out.println(request.toString());
 		
+		try {
+			Gson gson = new Gson();
+
+			Response response = client.newCall(request).execute();
+
+			if (response != null) {
+
+				String responseBody = response.body().string();
+				System.out.println(responseBody);
+				code = gson.fromJson(responseBody, AccessCode.class);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return code;
 	}
 }
